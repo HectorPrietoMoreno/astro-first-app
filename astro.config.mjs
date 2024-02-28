b02e22afec40e981f566d1544417ddaa4c34a9dd
Hector Prieto Moreno <hector.prieto.moreno@gmail.com>
2024-02-28 20:28:36 +0100

    feat: add completed blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://tiny-creponne-4bb2f6.netlify.app/",
+  integrations: [preact()]
+});
